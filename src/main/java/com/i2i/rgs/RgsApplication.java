77e1f290ed8d1fe9ef7ca86400bb532a24f8b97c
SaravanaMuthuKumarS <saravanamuthukumar.sivasubramanian@ideas2it.com>
2024-11-12 22:17:53 +0530

    Initialized setup for Report Genie Server

---
+++ src/main/java/com/i2i/rgs/RgsApplication.java
@@ -0,0 +1,13 @@
+package com.i2i.rgs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RgsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RgsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/i2i/rgs/RgsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.i2i.rgs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RgsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
